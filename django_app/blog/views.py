32f68ca59d166455d9b2a8a3cf98a500c1fa21d2
nehakerung <nehakerung@gmail.com>
2024-10-25 18:00:43 +0100

    Part 3 - Templates and css

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,8 +1,30 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Neha',
+        'title' : 'First Post',
+        'content': 'lorem ipsum',
+        'date': '25 august'
+    },
+    {
+        'author': 'Nagma',
+        'title': 'Second Post',
+        'content': 'lorem ipsum',
+        'date': '23 September'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request,
+                  'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About<h1>')
+    return render (request, 'blog/about.html',  {'title' : 'About'}
+                   )
