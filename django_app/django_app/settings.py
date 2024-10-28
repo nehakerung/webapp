27ac1e28ce6bdada075b28b7d1abf27193db7f6e
nehakerung <nehakerung@gmail.com>
2024-10-28 10:59:07 +0000

    Part 8 - User profile

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 from django.conf.global_settings import LOGIN_REDIRECT_URL
 
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
