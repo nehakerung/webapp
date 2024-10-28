57a2fda015571919391a411e1927c30e7757b368
nehakerung <nehakerung@gmail.com>
2024-10-28 16:17:11 +0000

    Part 10 - Create, Update and Delete Posts

--- django_app/users/views.py
+++ django_app/users/views.py
@@ -1,33 +1,35 @@
-from tarfile import USTAR_FORMAT
-
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
+
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = UserRegistrationForm,(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your Account has been created! You can now login')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
-        form = UserRegistrationForm()
+        form = UserRegistrationForm,()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES,
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
                                    instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your Account has been updated!')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
@@ -36,4 +38,5 @@ def profile(request):
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
