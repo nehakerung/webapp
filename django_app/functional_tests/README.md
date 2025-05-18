b98c83f380917389323ba36dbe76a110cc4b227e
nehakerung <nehakerung@gmail.com>
2025-05-18 19:12:49 +0100

    Add changes to Functional-tests

--- django_app/functional_tests/README.md
+++ django_app/functional_tests/README.md
@@ -1,1 +1,7 @@
 Functional Tests
+
+To run test, run in django_app:
+npx playwright test 
+
+To run CODEGEN to help build test, run the server and in another tab run:
+npx playwright codegen http://127.0.0.1:8000/
--- django_app/functional_tests/home_page.spec.js
+++ django_app/functional_tests/home_page.spec.js
@@ -1,7 +1,58 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has blog title', async ({page}) => {
+test('Home page has blog title', async ({page}) => {
+    // opens page
     await page.goto('http://127.0.0.1:8000/')
+    //Expected features of page
     await expect(page).toHaveTitle(/Django/);
+    await expect(page.getByText('Latest Posts')).toBeVisible;
+    await expect(page.getByText('Announcements')).toBeVisible;
+    await expect(page.getByText('Calendars')).toBeVisible;
+    await expect(page.getByText('etc')).toBeVisible;
+});
+
+test('The user can signs in', async ({page}) =>{
+    // opens page
+    await page.goto('http://127.0.0.1:8000/login/')
+    // User path to login
+    await page.getByRole('textbox', { name: 'Username' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('angelina');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password:' }).fill('mnbvcxz1.');
+    await page.getByRole('button', { name: 'Login'}).click();
+
+    // feature of page to be visible
+    expect(page.getByText('New Post').isVisible);
+})
+
+test('User can make post', async({ page }) => {
+    await page.goto('http://127.0.0.1:8000/login/')
+    // User path to login
+    await page.getByRole('textbox', { name: 'Username' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('angelina');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password:' }).fill('mnbvcxz1.');
+    await page.getByRole('button', { name: 'Login'}).click();
+
+    // user journey to post
+    await page.getByRole('link', { name: 'New Post' }).click();
+    await page.getByRole('textbox', { name: 'Title:' }).click();
+    await page.getByRole('textbox', { name: 'Title:' }).press('CapsLock');
+    await page.getByRole('textbox', { name: 'Title:' }).fill('May Day');
+    await page.getByRole('textbox', { name: 'Content:' }).click();
+    await page.getByRole('textbox', { name: 'Content:' }).press('CapsLock');
+    await page.getByRole('textbox', { name: 'Content:' }).fill('It\'s a day in may!');
+    await page.getByRole('button', { name: 'Post' }).click();
+
+    // check post shows up on page
+    await page.getByRole('link', { name: 'Django Blog' }).click();
+    await expect(page.getByText('May Day')).toBeVisible;
+}
+)
+
+// Fail test
+test('test', async ({ page }) => {
+  await page.goto('http://127.0.0.1:8000/');
+  await page.getByRole('link', { name: 'Does not exist' }).click();
 });
